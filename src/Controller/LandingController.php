69616b786343c3a3515f84109186a2d8c90feb8d
alvaroboni77 <alvarobonilla77@gmail.com>
2024-04-23 19:44:02 +0200

    list products without styling

---
+++ src/Controller/LandingController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Document\Product;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class LandingController extends AbstractController
+{
+    public function __construct(private DocumentManager $documentManager)
+    {
+    }
+
+    public function index():Response
+    {
+        $products = $this->documentManager->getRepository(Product::class)->findAll();
+
+        return $this->render('landing/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+}
