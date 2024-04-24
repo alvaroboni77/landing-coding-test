9fbd3a041d094d9910cbab211b0a08c57cf57842
alvaroboni77 <alvarobonilla77@gmail.com>
2024-04-24 14:34:42 +0200

    refactor controller and handle error

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -15,12 +15,29 @@ class LandingController extends AbstractController
 
     public function index():Response
     {
-        $products = $this->documentManager->getRepository(Product::class)->createQueryBuilder()
-            ->sort('salesRank', 'ASC')
-            ->getQuery()->execute();
+        try {
+            $products = $this->getSortedProducts();
+            if (empty($products)) {
+                throw new \Exception('No products found.');
+            }
+        } catch (\Exception $e) {
+            return $this->render('landing/error.html.twig', [
+                'message' => $e->getMessage()
+            ]);
+        }
 
         return $this->render('landing/index.html.twig', [
             'products' => $products
         ]);
+    }
+
+    private function getSortedProducts(): array
+    {
+        return $this->documentManager->getRepository(Product::class)
+            ->createQueryBuilder()
+            ->sort('salesRank', 'ASC')
+            ->getQuery()
+            ->execute()
+            ->toArray();
     }
 }
